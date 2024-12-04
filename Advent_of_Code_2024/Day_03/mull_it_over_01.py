e6ad1e32d04093fd9dd6c217980d7eceb5f10fcd
K. Kostov <122868401+konstakostov@users.noreply.github.com>
2024-12-04 15:15:31 +0200

    AoC, Day 3, Both Tasks completed

--- Advent_of_Code_2024/Day_03/mull_it_over_01.py
+++ Advent_of_Code_2024/Day_03/mull_it_over_01.py
@@ -3,13 +3,14 @@ import re
 mul_sum = 0
 
 # Read input file
-with open("mull_it_over_input.txt", "r") as data:
-    for memory in data:
-        matches = re.findall(r"mul\(\d+,\d+\)", memory)
+data_text = open("mull_it_over_input.txt").read()
 
-        for match in matches:
-            numbers = list(map(int, re.findall(r"(\d+)", match)))
+# Find all matches
+matches = re.findall(r"mul\(\d+,\d+\)", data_text)
 
-            mul_sum += numbers[0] * numbers[1]
+for match in matches:
+    numbers = list(map(int, re.findall(r"(\d+)", match)))
+
+    mul_sum += numbers[0] * numbers[1]
 
 print(mul_sum)
